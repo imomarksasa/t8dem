1315f1ca223a72ac876f896edd8df593f7c44376
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-09-22 03:26:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,26 @@ client.on('message', message => {
   	}
 });
 
-
+client.on('message',async function (message) =>  {
+const prefix = "#";
+if (message.content.startsWith(prefix+'تقديم'))
+    {
+        var members = []
+        let evidence = message.content.split(" ").slice(1,2).join(" ")
+        let reason = message.content.split(" ").slice(2).join(" ")
+        if (!reason) return message.reply(`**${prefix}apply [تجربه] [تجربه]**`)
+        if(!evidence.match(linkreg)) return message.channel.send(`**${prefix}apply [تجربه] [تجربه]**`)
+        if(!evidence) return message.reply(`راجاً انتضر رد الادارة لتقديمك`)
+        var embed = new Discord.RichEmbed()
+            .setTitle(`تقديم من ${message.author.username}`)
+            .addField(`التقديم`, "**`\`\`\"+ reason + "`\`\`\**")
+            .addField(`رابط المقطع`, evidence)
+            .setColor(`GREEN`)
+            client.channels.get("492265800347156500").send(embed)
+            members.push(message.author.id);
+            message.channel.send(`${mentions} تم تقديم طلبك...`)
+        }  
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
