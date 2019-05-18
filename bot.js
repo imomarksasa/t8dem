9981c39d9c50dc518cf12d102be02b898821a01a
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-05-19 01:07:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,92 @@ client.on('message', message => {
       }
 });
 
+const arf = JSON.parse(fs.readFileSync("./arf.json", "utf8"));
+let emojiss = require("node-emoji");//npm i node-emoji
+client.on("message", msg=>{
+if(msg.content.startsWith(`${prefix}setRole`)){//Toxic Codes n3k4a is one
+    if(!arf[msg.guild.id]) arf[msg.guild.id] = {
+        role: "Member"
+    }
+if(!msg.member.hasPermission("ADMINISTRATOR")) return msg.reply("you don't have permission").then(s => {s.delete(1600);})//Toxic Codes
+msg.reply("منشن الروم الي تبي فيه التفعيل").then(msgs=>{
+  const filter = response => response.author.id === msg.author.id;
+  msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })//Toxic Codes n3k4a is one
+  .then( collected =>{
+    msg.delete();
+    let idC = msg.guild.channels.find(c=>c.id == collected.first().mentions.channels.first().id)//Toxic Codes
+    if(!idC) return msgs.edit("لم اجد الروم");//Toxic Codes n3k4a is one
+     idC = idC.id;
+     msgs.edit(`${msg.author}, ادخل الايموجي الذي تريدة للتفعيل`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+.then( collected =>{
+if(!emojiss.hasEmoji(collected.first().mentions._content)) return msgs.edit("ادخل ايموجي صحيح !");
+newemoji = collected.first().mentions._content;
+msg.delete();
+msgs.edit(`${msg.author}, منشن للرتبة الذي تريدها`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })//Toxic Codes
+.then( collected =>{
+let roleW = collected.first().mentions.roles.first()
+if(!roleW) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - منشن الرتبة `);
+  msg.reply(embed).then( z => z.delete(3000)); return
+};
+let role = msg.guild.roles.find(`name`, roleW.name);//Toxic Codes
+if(!role) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - Could't find \`${roleW.name}\` role.`);//Toxic Codes
+msg.reply(embed).then( msgs => msgs.delete(3000));
+return
+}
+roleNew = role;
+arf[msg.guild.id].role = roleNew.name
+msgs.edit(`${msg.author}, ادخل النص الذي تريدة`)//Toxic Codes
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })//Toxic Codes
+.then( collected =>{
+stringNew = collected.first().mentions._content;
+let channel = msg.guild.channels.get(idC);
+if(!channel) {
+ let embed = new Discord.RichEmbed()
+ .setColor("#42f4f4")
+ .setTitle(`:x: - Could't find \`${idC}\` Channel.`);
+msg.reply(embed).then( msgs => msgs.delete(3000));
+return
+}
+channel.bulkDelete(100)
+channel.send(`@here || @everyone
+${msg.guild.name}© :arrow_down:
+ 
+${stringNew}
+`).then( msgA =>{
+msgA.react(newemoji).then(()=>{
+    let rolee = arf[msg.guild.id].role
+  const Ac = (reaction, user) => reaction.emoji.name === newemoji && !user.bot;
+  const Acc = msgA.createReactionCollector(Ac, {time: 120000});
+  Acc.on("collect", r=>{
+  let member = msg.guild.members.get(r.users.last().id);
+  if(!member) return;
+  r.remove(member.user);
+if(member.roles.find(r=>r.name == rolee)) return;
+    member.addRole(roleNew);
+  channel.send(`${member.user}, تم تفعيلك`).then(z => z.delete(1500));
+  fs.writeFile("./arf.json", JSON.stringify(arf), function(a) {
+      if (a) throw a;
+  })
+})})})
+}).catch(e => {console.log(e.message)});  //Toxic Codes
+}).catch(e => {console.log(e.message)});//Toxic Codes
+}).catch(e => {console.log(e.message)});//Toxic Codes
+}).catch(e => {console.log(e.message)});//Toxic Codes
+})
+///
+}});//Toxic Codes
+
 client.on('ready',  () => {
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
   console.log('by BadGuY');
